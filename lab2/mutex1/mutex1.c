017f293b1f73d4e47db406d1c0742b70cb13a2f3
pj <hpj@u.nus.edu>
2021-02-12 22:33:35 +0800

    sixth commit

--- lab2/mutex1/mutex1.c
+++ lab2/mutex1/mutex1.c
@@ -1,5 +1,5 @@
-// pthread2.c
-// This programme will create two threads, each print its own
+// mutex1.c
+// This programme demonstrate mutex
 // message onto the screen
 
 #include <stdio.h>
@@ -7,14 +7,17 @@
 #include <pthread.h>
 
 int g_var1 = 0;
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 
 void *inc_gv() {
     int i, j;
     for (i=0;i<10;i++) {
+        pthread_mutex_lock(&mutex1);
         g_var1++;
         for (j=0;j<5000000;j++);
         printf(" %d", g_var1);
         fflush(stdout);
+        pthread_mutex_unlock(&mutex1);
     }
     return NULL;
 }
