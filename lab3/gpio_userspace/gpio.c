b2875c2a0174cdc056e55b7c419310c1e0fd925e
pj <hpj@u.nus.edu>
2021-03-18 13:09:14 +0800

    24

--- lab3/gpio_userspace/gpio.c
+++ lab3/gpio_userspace/gpio.c
@@ -14,30 +14,34 @@ int main(int argc, char *argv[ ])
 	struct gpiochip_info cinfo;
 	ioctl(fd0,GPIO_GET_CHIPINFO_IOCTL,&cinfo); // get the chip information
 	fprintf(stdout, "GPIO chip 0: %s, \"%s\", %u lines\n", cinfo.name, cinfo.label, cinfo.lines);
-	struct gpiohandle_request req_GY; // Green and Yellow
-	struct gpiohandle_data data_GY; // for data bit
+	struct gpiohandle_request req_GYR; // Green and Yellow and Red
+	struct gpiohandle_data data_GYR; // for data bit
 	struct gpioevent_request req_pb; // for push button
 	struct gpioevent_data event;
-	req_GY.lines = 2; // 2 pins in this handler
-	req_GY.lineoffsets[0] = 4; //pin 4 - Green LED
-	req_GY.lineoffsets[1] = 17; // pin 17 - Yellow LED
-	req_GY.flags = GPIOHANDLE_REQUEST_OUTPUT; // set them to be output
+	req_GYR.lines = 3; // 3 pins in this handler
+	req_GYR.lineoffsets[0] = 4; //pin 4 - Green LED
+	req_GYR.lineoffsets[1] = 17; // pin 17 - Yellow LED
+	req_GYR.lineoffsets[2] = 27; // pin 27 - Red LED
+	req_GYR.flags = GPIOHANDLE_REQUEST_OUTPUT; // set them to be output
     req_pb.lineoffset = 11; //pin 11 push button
 	req_pb.handleflags = GPIOHANDLE_REQUEST_INPUT; 
     req_pb.eventflags = GPIOEVENT_REQUEST_RISING_EDGE; // using Rising Edge to trigger event
     strcpy(req_pb.consumer_label, "Push Button Event");
-	data_GY.values[0] = 1; // set initial value of Green LED to High (ON)
-	data_GY.values[1] = 0; // set initial value of Yellow LED to Low (OFF)
-	ioctl(fd0, GPIO_GET_LINEHANDLE_IOCTL, &req_GY); // now get the line handler req_GY
+	data_GYR.values[0] = 1; // set initial value of Green LED to High (ON)
+	data_GYR.values[1] = 0; // set initial value of Yellow LED to Low (OFF)
+	data_GYR.values[2] = 0; // set initial value of Red LED to Low (OFF)
+	ioctl(fd0, GPIO_GET_LINEHANDLE_IOCTL, &req_GYR); // now get the line handler req_GYR
     ioctl(fd0, GPIO_GET_LINEEVENT_IOCTL, &req_pb); // execute ioctl() to get the event handler req_pb
-	for (int i = 0; i < 5; ++i) {
-		ioctl(req_GY.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data_GY); // output data 
+	while (1) {
+		ioctl(req_GYR.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data_GYR); // output data 
         read(req_pb.fd, &event, sizeof(event));
 		usleep(1000000); //sleep for 1 second
-		data_GY.values[0] = !data_GY.values[0]; // toggle
-		data_GY.values[1] = !data_GY.values[1];
-	} //for
-	close(req_GY.fd); // release line
+		data_GYR.values[0] = !data_GYR.values[0]; // toggle
+		data_GYR.values[1] = !data_GYR.values[1];
+	} 
+
+	close(req_GYR.fd); // release line
+	close(req_pb.fd); // release line
 	close(fd0); // close the file
 	exit(EXIT_SUCCESS);
 }//main
